b8ff3c7bcebe21b9c1c243cca8a24b2fc4284e20
Izuku29 <89025886+Izuku29@users.noreply.github.com>
2021-10-01 17:15:52 +0530

    Linear search
    Using C programming and then iterating through the array. When the required value is found the loop breaks and the element is printed.

--- Linear Search/Linear search.c
+++ Linear Search/Linear search.c
@@ -5,19 +5,19 @@ int main()
   int a[n];
   printf("Enter the number of elements of array\n");
   scanf("%d",&n);
-  printf("Enter the array values\n");
+  printf("Enter the array values\n");//taking the array elements by using a for loop
   for (i=0;i<n;i++)
   {
     scanf("%d",&a[i]);
   }
   printf("Enter the value to be searched:");
-  scanf("%d",&v);
+  scanf("%d",&v); // value to be searched
   for (i=0;i<n;i++)
   {
     if(a[i]==v)
     {
       printf("Position of %d is %d",v,i);
-      break;
+      break;//breaks out of the loop
     }
   }
   return 0;
