62dff67288f61015b3fcd4bcefd2ae569766f1f2
vaibhav mishra <89639461+VaibhavM01@users.noreply.github.com>
2021-10-22 21:12:40 +0530

    Added Jump search.js
    Jump search in java script

---
+++ Jump search.js
@@ -0,0 +1,23 @@
+
+// Jump search in java script
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (sortedArray[middle] === key) {
+            // found the key
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            // continue searching to the right
+            start = middle + 1;
+        } else {
+            // search searching to the left
+            end = middle - 1;
+        }
+    }
+	// key wasn't found
+    return -1;
+}
